52456436af22a97ca8584300a4c7e389e0209da4
Sai Nane <esainane+git@gmail.com>
2024-12-14 05:08:37 +0000

    Use game-retrieved NetScriptDefinitions.d.ts
    Generated via `npx bitburner-filesync`
    
    Keep it checked in, though. I'll be interested to see how it changes
    over time.

--- NetScriptDefinitions.d.ts
+++ NetScriptDefinitions.d.ts
@@ -1924,62 +1924,39 @@ export interface Singularity {
 
   /**
    * Check if the player is busy.
-   *
    * @remarks
    * RAM cost: 0.5 GB * 16/4/1
    *
-   * Returns a boolean indicating if the player is currently performing an action. These actions include:
    *
-   * - Studying at a university
+   * Returns a boolean indicating whether or not the player is currently performing an
+   * ‘action’. These actions include working for a company/faction, studying at a university,
+   * working out at a gym, creating a program, committing a crime, etc.
    *
-   * - Working out at a gym
-   *
-   * - Working for a company/faction
-   *
-   * - Creating a program
-   *
-   * - Committing a crime
-   *
-   * - Grafting an augmentation
-   *
-   * Note:
-   *
-   * - This function returns true when the UI is showing the infiltration UI or the BitVerse.
-   *
-   * - This function does not take Bladeburner actions into account. You can use {@link Bladeburner.getCurrentAction | getCurrentAction}
-   *  to check if you are performing a Bladeburner action.
-   *
-   * @returns True if the player is currently performing an action, false otherwise.
+   * @returns True if the player is currently performing an ‘action’, false otherwise.
    */
   isBusy(): boolean;
 
   /**
    * Stop the current action.
-   *
    * @remarks
    * RAM cost: 1 GB * 16/4/1
    *
-   * This function stops the action the player is currently performing. The player will receive rewards (money,
-   * experience, etc.) they have earned from that action.
+   *
+   * This function is used to end whatever ‘action’ the player is currently performing.
+   * The player will receive whatever money/experience/etc. he has earned from that action.
    *
    * The actions that can be stopped with this function are:
    *
-   * - Studying at a university
+   * * Studying at a university
+   * * Working out at a gym
+   * * Working for a company/faction
+   * * Creating a program
+   * * Committing a crime
    *
-   * - Working out at a gym
+   * This function will return true if the player’s action was ended.
+   * It will return false if the player was not performing an action when this function was called.
    *
-   * - Working for a company/faction
-   *
-   * - Creating a program
-   *
-   * - Committing a crime
-   *
-   * - Grafting an augmentation
-   *
-   * This function will return true if the player's action is stopped. It will return false if the player is not
-   * performing an action when this function is called.
-   *
-   * @returns True if the player’s action is stopped, false if the player is not performing an action.
+   * @returns True if the player’s action was ended, false if the player was not performing an action.
    */
   stopAction(): boolean;
 
@@ -5419,7 +5396,7 @@ interface BladeburnerFormulas {
    *
    * @param name - Skill name. It's case-sensitive and must be an exact match.
    * @param level - Skill level. It must be a non-negative number.
-   * @param skillPoints - Number of skill points to upgrade the skill. It must be a non-negative number.
+   * @param skillPoints - Number of skill points to upgrade the skill. It must be a positive number.
    * @returns Number of times that you can upgrade the skill.
    */
   skillMaxUpgradeCount(
