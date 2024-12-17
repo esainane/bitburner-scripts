462db0308ddfa84589336845c34c1fda978ecd1d
Sai Nane <esainane+git@gmail.com>
2024-12-17 11:04:24 +0000

    mass.ts: Copy all of lib/ automatically

--- src/mass.ts
+++ src/mass.ts
@@ -3,36 +3,48 @@ import { find_servers } from 'lib/find-servers';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function autocomplete(data : AutocompleteData, args : string[]) : string[] {
-  return [...data.scripts];
+  return [...data.scripts, '--run', '--run-threads', '--no-lib'];
 }
 
 export async function main(ns: NS): Promise<void> {
   const servers: Array<string> = find_servers(ns);
 
-
+  const p_args = ns.args.map(String).filter(d => !d.startsWith('-'));
   // Copy script to all servers
-  const script = String(ns.args[0]);
-  if (!ns.fileExists(script)) {
-    ns.tprint(`Script not found: ${script}`);
-    return;
-  }
-  for (const target of servers) {
-    ns.scp(script, target);
-  }
-
-  // Try to run scripts: Might silently fail if we don't have access
-  if (ns.args[1] == '--run') {
-    for (const target of servers) {
-      ns.exec(script, target, 1, ...ns.args.slice(2));
-    }
-  } else if (ns.args[1] == '--run-threads') {
-    const scriptRam = ns.getScriptRam(script);
-    for (const target of servers) {
-      const threads = Math.floor((ns.getServerMaxRam(target) - ns.getServerUsedRam(target)) / scriptRam);
-      if (threads < 1) {
-        continue;
+  // First copy everything in lib/ unless expressly requested otherwise
+  if (ns.args.indexOf('--no-lib') === -1) {
+    const libs = ns.ls('home').filter(d=>d.startsWith('lib/'));
+    for (const file of libs) {
+      for (const target of servers) {
+        ns.scp(file, target);
       }
-      ns.exec(script, target, threads, ...ns.args.slice(2));
+    }
+  }
+  // Then the listed scripts
+  const scripts = p_args;
+  for (const script in scripts) {
+    if (!ns.fileExists(script)) {
+      ns.tprint(`Script not found: ${script}`);
+      return;
+    }
+    for (const target of servers) {
+      ns.scp(script, target);
+    }
+
+    // Try to run listed scripts if requested: Might silently fail if we don't have access
+    if (ns.args.indexOf('--run') !== -1) {
+      for (const target of servers) {
+        ns.exec(script, target, 1, ...ns.args.slice(2));
+      }
+    } else if (ns.args.indexOf('--run-threads') !== -1) {
+      const scriptRam = ns.getScriptRam(script);
+      for (const target of servers) {
+        const threads = Math.floor((ns.getServerMaxRam(target) - ns.getServerUsedRam(target)) / scriptRam);
+        if (threads < 1) {
+          continue;
+        }
+        ns.exec(script, target, threads, ...ns.args.slice(2));
+      }
     }
   }
 }
