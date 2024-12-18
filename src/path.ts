96cf1e42b193012dc3ab8aaf35ff0c35558429d1
Sai Nane <esainane+git@gmail.com>
2024-12-18 03:19:36 +0000

    path.ts: Find better paths
    The main feature here is being able to start from any server
    we've previously installed a backdoor on

--- src/path.ts
+++ src/path.ts
@@ -5,31 +5,58 @@ export function autocomplete(data : AutocompleteData, args : string[]) : string[
   return [...data.servers];
 }
 
+/**
+ * Returns true if the given server is one we can quickly start from:
+ *
+ * - the current server
+ * - home (via the "home" command)
+ * - any NPC server with a backdoor, we can directly connect to
+ */
+function starting_location(server: Server, here: Server): boolean {
+  return server === here || server.backdoorInstalled || server.hostname === 'home';
+}
+
 export async function main(ns: NS): Promise<void> {
-  const target = String(ns.args[0]);
+  const target_hostname = String(ns.args[0]);
   // Traverse the network
   const seen: Set<string> = new Set();
-  const home: Server = ns.getServer('home');
+  const target: Server = ns.getServer(target_hostname);
+  if (!target) {
+    ns.tprint(`ERROR Unknown server ${target_hostname}`);
+    return;
+  }
+  const here: Server = ns.getServer();
+
+  if (starting_location(target, here)) {
+    // Easy case: we're already (or can immediately be) there
+    ns.tprint('SUCCESS ', target_hostname);
+    return;
+  }
   interface ServerData {
     server: Server;
     path: Array<string>;
   }
-  const to_visit: Array<ServerData> = [{server: home, path: []}];
+  // Work backwards from the target
+  const to_visit: Array<ServerData> = [{server: target, path: []}];
   while (to_visit.length > 0) {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     const {server, path} = to_visit.pop()!;
     if (seen.has(server.hostname)) {
       continue;
     }
     seen.add(server.hostname);
     for (const adj_name of ns.scan(server.hostname)) {
-      if (adj_name == target) {
-        ns.tprint([...path, server.hostname, adj_name].join(' -> '));
-        return;
-      }
       if (seen.has(adj_name)) {
         continue;
       }
-      to_visit.push({server: ns.getServer(adj_name), path: [...path, server.hostname]});
+      const server = ns.getServer(adj_name);
+      // Finish if we've found a path from any server we can quickly start from
+      if (starting_location(server, here)) {
+        ns.tprint('SUCCESS ', [adj_name, ...path, target_hostname].join(' -> '));
+        return;
+      }
+      to_visit.push({server: ns.getServer(adj_name), path: [server.hostname, ...path]});
     }
   }
+  ns.tprint('ERROR Cannot find path to ', target_hostname);
 }
