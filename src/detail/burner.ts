8403e5ce78d34b0bae8ac08d70ae45bb1f54dca2
Sai Nane <esainane+git@gmail.com>
2024-12-26 09:22:54 +0000

    Refactor: Split out lib/burner
    Also split out consts; duplicates don't yet reference these yet

--- src/detail/burner.ts
+++ src/detail/burner.ts
@@ -1,21 +1,7 @@
-import { BladeburnerActionName, BladeburnerActionType, NS } from '@ns'
+import { NS } from '@ns'
 import { colors, format_number, print_table } from '/lib/colors';
 import { format_duration } from '/lib/format-duration';
-
-const ms_per_sec = 1000;
-const ms_per_min = 60 * ms_per_sec;
-
-interface ActionEntry {
-  type: BladeburnerActionType | `${BladeburnerActionType}`;
-  action: BladeburnerActionName | `${BladeburnerActionName}`;
-  level: number;
-  min_chance: number;
-  max_chance: number;
-  duration: number;
-  remaining: number;
-  rep_gain: number;
-  rep_gain_per_minute: number;
-}
+import { ActionEntry, bladeburner_actions_data } from '../lib/burner';
 
 const percent = `${colors.fg_cyan}%${colors.reset}`;
 
@@ -35,56 +21,7 @@ function print_entry(ns: NS, entry: ActionEntry) {
 }
 
 export async function main(ns: NS): Promise<void> {
-  type Action = [BladeburnerActionType | `${BladeburnerActionType}`, BladeburnerActionName | `${BladeburnerActionName}`];
-  type ActionList = Action[];
-  // Using enums directly, eg BladeburnerActionType.BlackOp, will cause the @ns import to be emitted, causing a failure
-  // when run in bitburner. So we directly use the string representation instead.
-  const general_actions: ActionList = ns.bladeburner.getGeneralActionNames().map(d => ['General', d]);
-  const contract_actions: ActionList = ns.bladeburner.getContractNames().map(d => ['Contracts', d]);
-  const operation_actions: ActionList = ns.bladeburner.getOperationNames().map(d => ['Operations', d]);
-  const blackops_actions: ActionList = ns.bladeburner.getBlackOpNames().map(d => ['Black Operations', d]);
-  const actions: ActionList = [
-    ...general_actions,
-    ...contract_actions,
-    ...operation_actions,
-    ...blackops_actions,
-  ];
-
-  const team_size_available = ns.bladeburner.getTeamSize();
-  // Ensure we use the maximum team size for all relevant operations
-  for (const [type, action] of actions) {
-    // Only ops and blackops use teams
-    if (!['Operations', 'Black Operations'].includes(type)) {
-      continue;
-    }
-    const team_size = ns.bladeburner.getTeamSize(type, action);
-    if (team_size < team_size_available) {
-      ns.bladeburner.setTeamSize(type, action, team_size_available);
-    }
-  }
-
-  const actions_data = actions.map(([type, action]) => {
-    // General and Black Operations tasks do not have levels
-    const level = ['General', 'Black Operations'].includes(type) ? 0 : ns.bladeburner.getActionCurrentLevel(type, action);
-    const [min_chance, max_chance] = ns.bladeburner.getActionEstimatedSuccessChance(type, action).map(d => d * 100);
-    const expected_chance = (min_chance + max_chance) / 2;
-    const duration = ns.bladeburner.getActionTime(type, action);
-    const remaining = ns.bladeburner.getActionCountRemaining(type, action);
-    const rep_gain = ns.bladeburner.getActionRepGain(type, action);
-    const rep_gain_per_minute = rep_gain * expected_chance / 100 / duration * ms_per_min;
-    return {
-      type,
-      action,
-      level,
-      min_chance,
-      max_chance,
-      duration,
-      remaining,
-      rep_gain,
-      rep_gain_per_minute,
-    };
-  });
-
+  const actions_data = bladeburner_actions_data(ns);
   // Sort by highest expected reputation gain per minute
   actions_data.sort((l, r) => r.rep_gain_per_minute - l.rep_gain_per_minute);
 
---
+++ src/lib/consts.ts
@@ -0,0 +1,4 @@
+
+export const ms_per_sec = 1000;
+export const ms_per_min = 60 * ms_per_sec;
+
