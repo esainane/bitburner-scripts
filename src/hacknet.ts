7649fb45f455fd74bfbbcaa46a11e566470f311d
Sai Nane <esainane+git@gmail.com>
2025-01-05 05:12:26 +0000

    hacknet: Less greedy bodge for new servers

--- src/hacknet.ts
+++ src/hacknet.ts
@@ -10,12 +10,76 @@ export function autocomplete(data: string[], args: string[]): string[] {
 
 const money_per_hash = 1e6 / 4;
 
+// Cache the result of the lookahead function to avoid recalculating it every tick.
+// Cache params are [new_server_cost, player_mults.hacknet_node_money, player_mults.hacknet_node_level_cost, player_mults.hacknet_node_ram_cost, player_mults.hacknet_node_core_cost]
+type NewServerLookaheadResult = [[number, number, number, number], number];
+let cache_params = [0,0,0,0,0];
+let cache_result: NewServerLookaheadResult = [[0,0,0,0],0];
+
+function array_eq<T>(l: T[], r: T[]): boolean {
+  return l.length === r.length && l.every((v, i) => v === r[i]);
+}
+
+function new_server_lookahead(ns: NS): NewServerLookaheadResult {
+  // New servers incur an exponentially scaling up-front cost, but often have a better payoff gradient if you can
+  // look ahead a few upgrades.
+
+  // This functions determines the tuple (levels, ram, cores) which maximises the cost efficiency of a new server.
+
+  // Properly, this is an Integer Linear Program, but we can get a "good enough" greedy solution by considering the
+  // cost efficiency of each upgrade in isolation.
+
+  // This function is pure with respect to these parameters:
+  const new_server_cost = ns.hacknet.getPurchaseNodeCost();
+  const player_mults = ns.getPlayer().mults;
+
+  // So if they haven't changed, return the cached result
+  if (array_eq(cache_params, [new_server_cost, player_mults.hacknet_node_money, player_mults.hacknet_node_level_cost, player_mults.hacknet_node_ram_cost, player_mults.hacknet_node_core_cost])) {
+    return cache_result;
+  }
+
+  // Track state of the best upgrade found so far. This tuple represents the number of ugprades, not the new value.
+  let best: [number, number, number, number] = [0, 0, 0, new_server_cost];
+  let best_hash_per_cost = ns.formulas.hacknetServers.hashGainRate(1, 0, 1, 1, player_mults.hacknet_node_money) / new_server_cost;
+
+  const add_candidate = (levels: number, ram: number, cores: number, cost: number) => {
+    const hash_gain_rate = ns.formulas.hacknetServers.hashGainRate(1 + levels, 0, 2 ** ram, 1 + cores, player_mults.hacknet_node_money);
+    const hash_per_cost = hash_gain_rate / cost;
+    if (hash_per_cost > best_hash_per_cost) {
+      best = [levels, ram, cores, cost];
+      best_hash_per_cost = hash_per_cost;
+      return true;
+    }
+    return false;
+  }
+
+  // Simulate repeatedly buying upgrades until the overall gradient no longer improves
+  let improvement = true;
+  while (improvement) {
+    improvement = false;
+    const [levels, ram, cores, cost] = best;
+    improvement ||= add_candidate(levels + 1, ram, cores, cost + ns.formulas.hacknetServers.levelUpgradeCost(levels + 1, levels + 2, player_mults.hacknet_node_level_cost));
+    improvement ||= add_candidate(levels, ram + 1, cores, cost + ns.formulas.hacknetServers.ramUpgradeCost(2 ** ram, 2 ** (ram + 1), player_mults.hacknet_node_ram_cost));
+    improvement ||= add_candidate(levels, ram, cores + 1, cost + ns.formulas.hacknetServers.coreUpgradeCost(cores + 1, cores + 2, player_mults.hacknet_node_core_cost));
+  }
+
+  // Update the cache
+  cache_params = [new_server_cost, player_mults.hacknet_node_money, player_mults.hacknet_node_level_cost, player_mults.hacknet_node_ram_cost, player_mults.hacknet_node_core_cost];
+  cache_result = [best, best_hash_per_cost];
+
+  return [best, best_hash_per_cost];
+}
+
 export async function main(ns: NS): Promise<void> {
   const do_sell = !ns.args.includes('--no-sell');
   const do_buy = !ns.args.includes('--no-buy');
   // Greedy algorithm go!
   // Work out the step with the greatest cost efficiency available, and make it, if possible.
   // If the best move cannot be afforded yet, sleep and retry later.
+  // TODO: This could be improved by determining if any cheap upgrades would pay for themselves in the time it takes to
+  // afford the highest ROI upgrade. This is a bit complicated, as we're unlikely to be the only earner, and we also
+  // make things other than money (and the first few coding contracts can easily outperform constant money purchase),
+  // but it would be nice to have the option.
   interface Action {
     cb: () => void;
     cost: number;
@@ -44,15 +108,28 @@ export async function main(ns: NS): Promise<void> {
     const nodes = ns.hacknet.numNodes();
     let best: Action | null = null;
     if (nodes < ns.hacknet.maxNumNodes()) {
-      const money_gain_delta = ns.formulas.hacknetServers.hashGainRate(1, 0, 1, 1, player_mult);
+      const hash_gain_delta = ns.formulas.hacknetServers.hashGainRate(1, 0, 1, 1, player_mult);
       const cost = ns.hacknet.getPurchaseNodeCost();
-      best = {
-        cost: cost,
-        hash_gain_delta: money_gain_delta,
-        cb: () => ns.hacknet.purchaseNode(),
-        description: `Purchase new node for ${currency_format(cost)}`
+      if (nodes === 0) {
+        // If we don't have any existing nodes, don't overcomplicate the calculations
+        best = {
+          cost: cost,
+          hash_gain_delta,
+          cb: () => ns.hacknet.purchaseNode(),
+          description: `Purchase new node for ${currency_format(cost)}`
+        }
+      } else {
+        // If we do have any existing nodes, look ahead in case delayed gratification would pay off
+        const [upgrade, hash_per_cost] = new_server_lookahead(ns);
+        best = {
+          cost,
+          hash_gain_delta: hash_per_cost * upgrade[3],
+          cb: () => ns.hacknet.purchaseNode(),
+          description: `Purchase new node for ${currency_format(cost)}, looking ahead to performance at [${upgrade[0]} levels, ${upgrade[1]}GB RAM, ${upgrade[2]} cores] for ${currency_format(upgrade[3])}`
+        }
       }
     }
+    // Then, examine the effect of each available single upgrade for every existing node
     const add_candidate: (c: Action) => void = (c: Action) => {
       if (best == null) {
         best = c;
@@ -62,7 +139,7 @@ export async function main(ns: NS): Promise<void> {
         best = c;
       }
     }
-    for (let idx=0; idx < nodes; ++idx) {
+    for (const idx of range(nodes)) {
       const stats = ns.hacknet.getNodeStats(idx);
       const current_rate = ns.formulas.hacknetServers.hashGainRate(stats.level, 0, stats.ram, stats.cores, player_mult);
       add_candidate({
