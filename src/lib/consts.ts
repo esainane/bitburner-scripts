b627a97643436e7ef188236dd28192b65aa69eb4
Sai Nane <esainane+git@gmail.com>
2025-01-14 11:07:23 +0000

    HWGW workers: Configurable, larger warn threshold

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -3,3 +3,5 @@ export const ms_per_sec = 1000;
 export const ms_per_min = 60 * ms_per_sec;
 export const ms_per_hour = 60 * ms_per_min;
 export const ms_per_day = 24 * ms_per_hour;
+
+export const hwgw_delay_warn_threshold = -3e2;
--- src/worker/hack1.ts
+++ src/worker/hack1.ts
@@ -1,11 +1,12 @@
 import { BasicHGWOptions, NS } from '@ns'
 import { format_duration } from 'lib/format-duration';
 import { format_number, format_servername } from 'lib/colors';
+import { hwgw_delay_warn_threshold } from '/lib/consts';
 
 export async function main(ns: NS): Promise<void> {
   const target = String(ns.args[0]);
   const wait = Number(ns.args[1] ?? 0);
-  if (wait < -1e-3) {
+  if (wait < hwgw_delay_warn_threshold) {
     ns.tprint(`WARNING: large negative wait time ${format_duration(wait)} time for hack(${format_servername(target, {is_warning: true})}), setting to ${format_number(0)}`);
   }
   const opts: BasicHGWOptions = {
