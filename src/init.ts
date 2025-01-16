a1dfa01f502d1425ca862dd81501f99afc6ba012
Sai Nane <esainane+git@gmail.com>
2025-01-16 12:29:47 +0000

    init: Resequence startup
    - Try to buy key programs twice, for the cases where we do and don't
      have sources of soft-reset resilient income.
    - Start up corporation management if we have one going.
    - Start up hacknet, going all out to fuel a corporation if we have one
      or with a relatively conservative window for plain money otherwise.

--- src/init.ts
+++ src/init.ts
@@ -1,6 +1,15 @@
 import { NS } from '@ns'
 import { range } from '/lib/range';
 import { singularity_async } from '/lib/singu';
+import { SingularityAsync } from '/lib/singu-interface';
+
+async function purchase_key_programs(ns: NS, singu: SingularityAsync): Promise<void> {
+  // Purchase key programs, as much as we can
+  await singu.purchaseTor();
+  for (const prog of ['BruteSSH.exe', 'FTPCrack.exe', 'relaySMTP.exe', 'HTTPWorm.exe', 'SQLInject.exe']) {
+    await singu.purchaseProgram(prog);
+  }
+}
 
 export async function main(ns: NS): Promise<void> {
   ns.ramOverride(26.25);
@@ -26,21 +35,35 @@ export async function main(ns: NS): Promise<void> {
       ns.sleeve.setToUniversityCourse(i, 'Rothman University', 'Algorithms');
     }
   }
-  // Purchase key programs, as much as we can
-  await singu.purchaseTor();
-  for (const prog of ['BruteSSH.exe', 'FTPCrack.exe', 'relaySMTP.exe', 'HTTPWorm.exe', 'SQLInject.exe']) {
-    await singu.purchaseProgram(prog);
-  }
-  // Root everything we can
-  await ns.asleep(300);
-  ns.exec('root.js', 'home');
+
+  // In case we don't have sources of ongoing income (gang, corp), we must ensure starting funds for key programs first
+  await purchase_key_programs(ns, singu);
+
   // Distribute scripts everywhere
   await ns.asleep(300);
   ns.exec('mass.js', 'home');
   await ns.asleep(300);
   // Start up the management scripts
   ns.exec('gang.js', 'home');
+  if (ns.corporation.hasCorporation()) {
+    ns.exec('corp.js', 'home');
+    ns.exec('hacknet.js', 'home', 1, '--corp');
+  } else {
+    // Conservative-ish configuration: any upgrade which pays for itself in an hour and a half
+    // There really should be a sliding window option based on how long it has been since soft reset
+    ns.exec('hacknet.js', 'home', 1, 5400);
+  }
+
   // Give ourselves time to get any very easy levels before starting up the autohack scheduler
-  await ns.asleep(5000);
+  await ns.asleep(10000);
+
+  // In case we do have access to ongoing income, try to buy everything again after we've had time for a cycle or two
+  await purchase_key_programs(ns, singu);
+
+  // Root everything we can
+  ns.exec('root.js', 'home');
+
+  // Finally, start up autohacking
+  await ns.asleep(300);
   ns.spawn('omni.js', 1, '--quiet');
 }
